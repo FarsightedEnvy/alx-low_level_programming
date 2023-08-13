c95dcdbd1b4cf9c5cb9156242c8469c0d1f594fc
FarsightedEnvy <keegansaunders49@gmail.com>
2023-08-13 09:49:42 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * Return: each time 0 (success)
+ */
+
+int main(void)
+{
+	char k;
+
+	k = 'a';
+	while
+		(k <= 'z') {
+			if ((k != 'q' && k != 'e') && k <= 'z')
+				putchar(k);
+			k++;
+		}
+	putchar('\n');
+	return (0);
+}
