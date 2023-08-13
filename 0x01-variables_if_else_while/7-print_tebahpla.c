8e5625b88c200c8b265ca28405f7e2068155d57d
FarsightedEnvy <keegansaunders49@gmail.com>
2023-08-13 10:28:56 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * Return: each time 0 (success)
+ */
+
+int main(void)
+{
+	int k;
+
+	k = 'z';
+	while
+		(k >= 'a') {
+			putchar(k);
+			k--;
+		}
+	putchar('\n');
+	return (0);
+}
