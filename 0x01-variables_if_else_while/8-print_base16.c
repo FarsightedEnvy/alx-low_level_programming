eade8b173eac1d7817f8175f39ca6744b1bb2774
FarsightedEnvy <keegansaunders49@gmail.com>
2023-08-13 10:45:35 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * Retun: each time 0 (success)
+ */
+
+int main(void)
+{
+	char k;
+
+	int s;
+
+	k = 'a';
+	s = 0;
+	while 
+		(s < 10) {
+			putchar(s + '0');
+			s++;
+		}
+	while 
+		(k <= 'f') {
+			putchar(k);
+			k++;
+		}
+	putchar('\n');
+	return (0);
+}
