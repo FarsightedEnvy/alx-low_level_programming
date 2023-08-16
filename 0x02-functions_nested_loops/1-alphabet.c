639f0729e291f168f5d72720ba440b435a453639
FarsightedEnvy <keegansaunders49@gmail.com>
2023-08-16 11:56:38 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - for this task at hand this is the function
+ * Return: each time 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	char k;
+
+	for (k = 'a'; k <= 'z'; k++)
+	{
+		_putchar(k);
+	}
+	_putchar('\n');
+}
