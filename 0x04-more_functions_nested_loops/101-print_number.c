407f8d27da4a2503b2c7fb091280b6b5c2fdc2a9
FarsightedEnvy <keegansaunders49@gmail.com>
2023-08-18 07:38:06 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - function displays an integer
+ * @n: the integer to be printed
+ */
+
+void print_number(int n)
+{
+	unsighted int k = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		k = -k;
+	}
+
+	if ((k / 10) > 0)
+		print_number(k / 10);
+
+	_putchar((k % 10) + '0');
+}
