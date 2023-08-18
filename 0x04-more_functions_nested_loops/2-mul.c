3bf149e71c80c352897f74c4913cbbe4f33e39e9
FarsightedEnvy <keegansaunders49@gmail.com>
2023-08-18 02:56:02 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - this function validates a digit (0 through)
+ * @a: a - variable
+ * @b: b - variable
+ * Return: each time 0
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
