8da2426fbad75d972009d7c2706476b959ed53ac
FarsightedEnvy <keegansaunders49@gmail.com>
2023-08-22 04:50:48 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - outputs a string followed by a new line
+ * to the standard output
+ * @str: print string
+ */
+
+void _puts(chat *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
