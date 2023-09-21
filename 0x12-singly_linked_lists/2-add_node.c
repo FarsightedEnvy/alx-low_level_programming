365526c91082ab22d234e3a6849951ebc08b29c6
FarsightedEnvy <keegansaunders49@gmail.com>
2023-09-21 15:31:16 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,19 +8,20 @@
  * @str: fresh str to insert into the node
  * Return: the address of the new element, or NULL if it fails
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	unsigned int len = 0;
+	unsigned int l = 0;
 
-	while (str[len])
-		len++;
+	while (str[l])
+		l++;
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->len = len;
+	new->l = l;
 	new->next = (*head);
 	(*head) = new;
 
